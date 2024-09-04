df4c3c1cccb90ec317fcd44dec3baedb984bb5ff
Felipe Ulson Sora <felipeusora@gmail.com>
2024-09-04 16:05:33 -0300

    Criando função que muda o título da página

--- carro-seguro/src/componentes/Cabecalho/Cabecalho.tsx
+++ carro-seguro/src/componentes/Cabecalho/Cabecalho.tsx
@@ -1,13 +1,28 @@
-
+import { useLocation } from 'react-router-dom';
 import './Cabecalho.css';
 import Menu from '../Menu/Menu';
 
 export default function Cabecalho() {
-    
+    const location = useLocation();
+
+    const getTitulo = () => {
+        switch (location.pathname) {
+            case '/diagnostico':
+                return 'Diagnóstico de Problemas';
+            case '/faq':
+                return 'FAQ de Manutenção';
+            case '/contato':
+                return 'Contato';
+            case '/integrantes':
+                return 'Integrantes';
+            default:
+                return 'Bem-vindo à CarroSeguro';
+        }
+    };
 
     return (
         <header>
-            <h1 className="titulo_cabecalho">Titulo</h1>
+            <h1 className="titulo_cabecalho">{getTitulo()}</h1>
             <Menu/>
         </header>
     );
