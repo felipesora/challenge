f3fee354fe769158e8ead674557a80f1498689de
felipesora <felipeusora@gmail.com>
2024-09-13 16:38:04 -0300

    css conta

--- carro-seguro/src/routes/Conta/Conta.tsx
+++ carro-seguro/src/routes/Conta/Conta.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './conta.css'
 
 export default function Conta() {
   const [user, setUser] = useState<any>(null);
@@ -15,15 +16,18 @@ export default function Conta() {
   }
 
   return (
-    <div>
-      <h1>Minha Conta</h1>
-      <ul>
-        <li><strong>Nome:</strong> {user.nome}</li>
-        <li><strong>Email:</strong> {user.email}</li>
-        <li><strong>CPF:</strong> {user.cpf}</li>
-        <li><strong>Placa:</strong> {user.placa}</li>
-        <li><strong>Senha:</strong> {user.senha}</li> {}
-      </ul>
+    <div id="container_main_conta">
+      <div id="container_conta">
+        <h1 className="titulo_conta">Minha Conta</h1>
+        <p className="texto_conta">Abaixo estão seus dados cadastrados no sistema:</p>
+        <ul className="ul_conta">
+          <li><strong>Nome:</strong> {user.nome}</li>
+          <li><strong>Email:</strong> {user.email}</li>
+          <li><strong>CPF:</strong> {user.cpf}</li>
+          <li><strong>Placa:</strong> {user.placa}</li>
+          <li className="ultimo_li"><strong>Senha:</strong> {user.senha}</li> {}
+        </ul>
+      </div>
     </div>
   );
 }
